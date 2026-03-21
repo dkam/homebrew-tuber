90d93c89d46bb547e16dd2c5501e1be23c1a924f
Dan Milne <d@nmilne.com>
2026-03-21 17:34:12 +1100

    Version bumps for tuber and tuber-tui

--- Formula/tuber-tui.rb
+++ Formula/tuber-tui.rb
@@ -1,28 +1,28 @@
 class TuberTui < Formula
   desc "Terminal UI for tuber work queue"
   homepage "https://github.com/tuberq/tuber-tui"
-  version "0.1.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.1.0/tuber-tui-aarch64-apple-darwin.tar.gz"
-      sha256 "87bd9cacb149f55d89c12daa6b528d20ac96887c1acb689c45c340967783282d"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.3.0/tuber-tui-aarch64-apple-darwin.tar.gz"
+      sha256 "5c5f26278b768a9964ff5ae188dd10c39515169dce11d864898efd2d73743684"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.1.0/tuber-tui-x86_64-apple-darwin.tar.gz"
-      sha256 "27115e4fabc7cc6d378755bdefdb81f4d121e366e3515c0ea19e0fa6a79a7ef3"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.3.0/tuber-tui-x86_64-apple-darwin.tar.gz"
+      sha256 "c18e6ff7ab4030f5d41f7986f602b142fda6d849a685aed0fea380f4ecb3cf50"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.1.0/tuber-tui-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "d9bf1432b1271abea9f3f41f51c220739e2fe80ae46d19e53f8c43e14d0eade9"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.3.0/tuber-tui-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "b12ebe68d37d44728957c6ebe36506227deb4dbc018c6fa1810e990c4c0ac10c"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.1.0/tuber-tui-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "fa234b4f68866c1d18d15e36247ab211126cf084753ab96659ea1678aa44b39b"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.3.0/tuber-tui-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "654e3528d6189a85cc88db182cc57796c756f73b538baf29c871210230d90948"
     end
   end
 
--- Formula/tuber.rb
+++ Formula/tuber.rb
@@ -1,28 +1,28 @@
 class Tuber < Formula
   desc "A fast work queue - Rust rewrite of beanstalkd"
   homepage "https://github.com/tuberq/tuber"
-  version "0.2.11"
+  version "0.3.3"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tuberq/tuber/releases/download/v0.2.11/tuber-aarch64-apple-darwin.tar.gz"
-      sha256 "ee7bb68ccd0b29c5bf631bd7602f26a3e6edad8627596beac7ac1720bf9d0712"
+      url "https://github.com/tuberq/tuber/releases/download/v0.3.3/tuber-aarch64-apple-darwin.tar.gz"
+      sha256 "e8926fa8e936107d6b32c7a2037b4a367f323e1f2b8b062ec979fce42287f215"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber/releases/download/v0.2.11/tuber-x86_64-apple-darwin.tar.gz"
-      sha256 "9ae0d6046177ffab8e1a9e1676bdd951927e42fac08695cdab3915cddce0f43f"
+      url "https://github.com/tuberq/tuber/releases/download/v0.3.3/tuber-x86_64-apple-darwin.tar.gz"
+      sha256 "36f3adcb769d971101df02a3d30161bfe611159c6373a538e98c85377770a2ab"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/tuberq/tuber/releases/download/v0.2.11/tuber-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "e1d4517b07b2d46c64afcbf969dbca8bd95cda92db2c668e3b3b6830e6d0992b"
+      url "https://github.com/tuberq/tuber/releases/download/v0.3.3/tuber-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "01dff82b345eba0f57a1eebbe7d065610ce49a0f30adb3d0241e605878ded428"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber/releases/download/v0.2.11/tuber-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "6b89eefab6f9ae682c8d068fecf2410063d6289bf532f798867a56fe28130c34"
+      url "https://github.com/tuberq/tuber/releases/download/v0.3.3/tuber-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "ab271ec5446bf87c580a59ef7b074ca1953e3809321749b7af2a354ee3239c70"
     end
   end
 
