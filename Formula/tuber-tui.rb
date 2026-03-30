19d48ddc22d9099895e3fec489681728de3a12a7
Dan Milne <d@nmilne.com>
2026-03-30 22:24:56 +1100

    Arrows!

--- Formula/tuber-tui.rb
+++ Formula/tuber-tui.rb
@@ -1,28 +1,28 @@
 class TuberTui < Formula
   desc "Terminal UI for tuber work queue"
   homepage "https://github.com/tuberq/tuber-tui"
-  version "0.5.4"
+  version "0.5.6"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.5.4/tuber-tui-aarch64-apple-darwin.tar.gz"
-      sha256 "d85e1f5f3b57180588a98e469287c00021dfdc3b7e77e97e0626b4ced44761e2"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.5.6/tuber-tui-aarch64-apple-darwin.tar.gz"
+      sha256 "fb2ee0da3877482a00fc41baf3fabc015691a6bf6d331db6d7a670b8bf8dfd73"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.5.4/tuber-tui-x86_64-apple-darwin.tar.gz"
-      sha256 "28299249403b6280481debdf105a748b48a5678e58510d5bb6eb5a71e5a40999"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.5.6/tuber-tui-x86_64-apple-darwin.tar.gz"
+      sha256 "46e8f5b8220f26c24a5af58e784157ed9c6fd7689e6183daa8602bbc8be28d87"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.5.4/tuber-tui-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "29a302a5f6c7c5af1fe17e7c35ea8351a24a0872d24e0b7971aa2e06443334b2"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.5.6/tuber-tui-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "ca0a073f77351035f93a21f3db9218ac4361ff3dfe3cdce76a6549c2bcdb35cf"
     end
     on_intel do
-      url "https://github.com/tuberq/tuber-tui/releases/download/v0.5.4/tuber-tui-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "687be1c459c4536ec29f37571f13d6c301642c07ea59a0c27cb66374475d858e"
+      url "https://github.com/tuberq/tuber-tui/releases/download/v0.5.6/tuber-tui-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "d174b5edfeaeca55fe527ad7dcaaf5764ff2c38e55e22032aef72d7837838b11"
     end
   end
 
